ccdbb9e1e78e1d352186a9932fc64fd418e6f44f
younsoo <tweatershin@gmail.com>
2023-01-02 10:23:37 +0900

    # Init Project

--- board/views.py
+++ board/views.py
@@ -2,10 +2,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    # print(request)
-    # print(type(request))
-    print(dir(request))
-
-
-
     return HttpResponse("Hello World, I am Younsoo!")
