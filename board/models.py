7869f080b410bd4116f0908728b58b0ea5ba32dd
younsoo <tweatershin@gmail.com>
2023-01-02 13:35:58 +0900

    # django first model

--- board/models.py
+++ board/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Board(models.Model):
+    id = models.AutoField(primary_key=True)  # integer(auto-increment)
+    title = models.CharField(max_length=255)  # varchar(255)
+    content = models.TextField()  # Text
+    created_at = models.DateTimeField(auto_now_add=True)  # datetime
+    updated_at = models.DateTimeField(auto_now=True)  #
