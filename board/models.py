ed498a2ec9470877761e41b09ef349e1d70179ae
younsoo <tweatershin@gmail.com>
2023-01-03 10:51:44 +0900

    no message

--- board/models.py
+++ board/models.py
@@ -7,3 +7,10 @@ class Board(models.Model):
     content = models.TextField()  # Text
     created_at = models.DateTimeField(auto_now_add=True)  # datetime
     updated_at = models.DateTimeField(auto_now=True)  #
+
+
+class Comment(models.Model):
+    board = models.ForeignKey("Board", null=True, on_delete=models.SET_NULL)
+    content = models.CharField(max_length=255)# varchar(255)
+    created_at = models.DateTimeField(auto_now_add=True)  # datetime
+    updated_at = models.DateTimeField(auto_now=True)  #
