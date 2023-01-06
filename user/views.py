044c958521237d0712f645dbaab6232a866f26f1
younsoo <tweatershin@gmail.com>
2023-01-06 11:46:31 +0900

    no message

--- user/views.py
+++ user/views.py
@@ -15,7 +15,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from datetime import datetime
+
 
 # Create your views here.
 def index(request):
+    if not request.session.get('history'):
+        request.session['history'] = []
+    request.session['history'] = request.session['history'] + [datetime.now().strftime('%H:%M:%S')]
+    print(request.session['history'])
+    # print(request.session)
+    # print("session 확인")
+    # for k, v in request.session.items():
+    #     print(k, v)
+    # print(request.user)
+    # print(request.user.is_authenticated)
+    # print(request.user.is_superuser)
+    # print(request.user.is_staff)
+    # print(request.user.email)
     return render(request, 'user/profile.html')
